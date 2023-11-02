b576873f1cdbd6ab9d6ffe5e763ec5f46088ef25
Ansh Gupta <83304720+Anshg1109@users.noreply.github.com>
2023-11-02 20:37:17 +0530

    fixed code error

--- model/palindrome.py
+++ model/palindrome.py
@@ -1,12 +1,12 @@
 from car import Car
 from engines.sternman_engine import SternmanEngine
-from batteries.nubbin_battery import NUBBIN_BATTERY
+from batteries.spindler_battery import SpindlerBattery
 
 class Palindrome(Car):
 
-    def __init__(self,current_mileage, last_service_mileage, last_service_date):
-        palindrome_engine = SternmanEngine(current_mileage, last_service_mileage)
-        palindrome_battery = NUBBIN_BATTERY(last_service_date)
+    def __init__(self,warning_light_on, last_service_date):
+        palindrome_engine = SternmanEngine(warning_light_on)
+        palindrome_battery = SpindlerBattery(last_service_date)
         
         super().__init__(palindrome_engine, palindrome_battery)
         self.battery = palindrome_battery
